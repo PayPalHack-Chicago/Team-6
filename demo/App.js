c96d9e2296dbb1e170115a4b934498443fbbb0fc
Edwin Zamudio <ezamudio@hawk.iit.edu>
2019-10-12 11:29:34 -0500

    git

--- demo/App.js
+++ demo/App.js
@@ -1,5 +1,5 @@
 import React, {Component}from 'react';
-import { Button, Platform, Image, View, Text,TouchableOpacity, StyleSheet, Alert,TextInput } from 'react-native';
+import { Button, Platform, Image, View, Text,TouchableOpacity, StyleSheet, Alert,TextInput, FlatList} from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
@@ -7,7 +7,7 @@ import { createStackNavigator } from 'react-navigation-stack';
 class LogoTitle extends React.Component {
   render() {
     return (
-      <Text style ={{flex: 1,fontSize:20,fontWeight: 'bold',textAlign: 'left',color:'#303030'}}>FINLIT.</Text>
+      <Text style ={{flex: 1,fontSize:20,fontWeight: 'bold',textAlign: 'left',color:'#333333'}}>FIN U</Text>
     );
   }
 }
@@ -18,7 +18,7 @@ class Title extends React.Component {
   render() {
     return (
       <Image source={{uri: 'https://img.icons8.com/cotton/64/000000/receive-euro.png'}}
-        style={{ width: 45, height: 45 }}/>
+        style={{ width: 45, height: 46 }}/>
     );
   }
 }
@@ -36,61 +36,56 @@ class HomeScreen extends React.Component {
     return (
       <View style={{ flex: 1,justifyContent:'space-around'}}>
       <View style={{flex: 1, alignItems: 'center',justifyContent:'center'}}>
-        <Text style ={{fontSize:20,fontWeight: 'bold',color:'#303030'}}>Get to Know money, Before having money.</Text>
-        <Image source={{uri: 'https://us.123rf.com/450wm/dolgachov/dolgachov1610/dolgachov161008160/64336794-business-people-success-and-fortune-concept-happy-businessman-with-heap-of-dollar-money-at-office.jpg?ver=6'}}
-          style={{ width: 200, height: 100 }}/>
+        <Text style ={{fontSize:35,fontWeight: 'bold',color:'#333333',textAlign: 'center'}}>Premier College Wealth Engine.</Text>
+        <Text style ={{fontSize:18,textAlign:'center'}}>      </Text>
+        <Text style ={{fontSize:15,textAlign: 'center'}}>Learning a couple facts about financial literacy a day will make you a master of managing your finances. </Text>
+        <Text style ={{fontSize:18,textAlign:'center'}}>      </Text>
         <TouchableOpacity onPress = {() => {this.props.navigation.navigate('MyModal', {
           itemId: 86,
-          otherParam: 'FINLIT',
+          otherParam: 'FIN U',
         });}}>
-                <View style = {{height: 50,width: 100, backgroundColor: '#025928', alignItems: 'stretch',
+                <View style = {{height: 50,width: 100, backgroundColor: '#54b948', alignItems: 'stretch',
                                 justifyContent: 'flex-start', borderRadius: 15}}
                        >
-                    <Text style = {{flex: 1,color: 'white',textAlign: 'center',textAlignVertical:'center'}}>Why FINLIT?</Text>
+                    <Text style = {{flex: 1,color: '#FFFFFF',textAlign: 'center',textAlignVertical:'center'}}>Why FIN U?</Text>
                 </View>
           </TouchableOpacity>
       </View>
-  <View style={{ flex: 1,alignItems:'center', justifyContent: 'center', flexDirection:'row',alignContent:'stretch', justifyContent:'space-around'}}>
+      <Text style ={{fontSize:18,textAlign:'center'}}>      </Text>
+      <Text style ={{fontSize:18,textAlign:'center'}}>      </Text>
+  <View style={{ flex: 1,alignItems:'center', justifyContent: 'center', flexDirection:'row', justifyContent:'space-around'}}>
         <TouchableOpacity onPress = {() => {this.props.navigation.navigate('Details', {
               itemId: 86,
               otherParam: 'Compound Intrest',
             });}}>
-                <View style = {{flex:2, height: 150,width:100, backgroundColor: '#025928', borderRadius: 5}}>
-                    <Text style = {{flex: 1,alignSelf:'center',color: 'white',textAlignVertical: 'center',textAlign:'center'}}>Compound Intrests</Text>
+                <View style = {{flex:1, height: 20,width:100, backgroundColor: '#54b948', borderRadius: 7}}>
+                    <Text style = {{flex: 1,alignSelf:'center',color: 'white',textAlignVertical: 'center',textAlign:'center'}}>Compound Intrest</Text>
                 </View>
           </TouchableOpacity>
-
-          <TouchableOpacity onPress = {() => {this.props.navigation.navigate('Details2', {//change
-              itemId: 86,
-              otherParam: 'Amortization',
-            });}}>
-                  <View style = {{flex:2,height: 150, width:100,backgroundColor: '#025928', alignItems: 'stretch',
-                                  justifyContent: 'flex-start', borderRadius: 5}}
-                         >
-                      <Text style = {{flex: 1, color: 'white',textAlign: 'center',textAlignVertical:'center'}}>Amortization</Text>
+          <TouchableOpacity onPress = {() => {this.props.navigation.navigate('Extras', {
+                itemId: 86,
+                otherParam: 'More formulas',
+              });}}>
+                  <View style = {{flex:2, height: 150,width:100, backgroundColor: '#54b948', borderRadius: 7}}>
+                      <Text style = {{flex: 1,alignSelf:'center',color: 'white',textAlignVertical: 'center',textAlign:'center'}}>M1 Formulas</Text>
                   </View>
             </TouchableOpacity>
-            <TouchableOpacity onPress = {() => {this.props.navigation.navigate('Extras', {//change
-              itemId: 86,
-              otherParam: 'Extras',
-            });}}>
-                    <View style = {{flex:2,height: 150,width:100, backgroundColor: '#025928', alignItems: 'stretch',
-                                    justifyContent: 'flex-start', borderRadius: 5}}
-                           >
-                        <Text style = {{flex: 1,color: 'white',textAlign: 'center',textAlignVertical:'center'}}>Links and More indepth</Text>
-                    </View>
-              </TouchableOpacity>
               <TouchableOpacity onPress = {() => {this.props.navigation.navigate('Extras2', {//change
                 itemId: 86,
                 otherParam: 'Extras',
               });}}>
-                      <View style = {{flex:2,height: 150,width:100, backgroundColor: '#025928', alignItems: 'stretch',
-                                      justifyContent: 'flex-start', borderRadius: 5}}
+                      <View style = {{flex:2,height: 150,width:100, backgroundColor: '#54b948', alignItems: 'stretch',
+                                      justifyContent: 'flex-start', borderRadius: 7}}
                              >
                           <Text style = {{flex: 1,color: 'white',textAlign: 'center',textAlignVertical:'center'}}>College student Tips</Text>
                       </View>
                 </TouchableOpacity>
+
     </View>
+    <Text style ={{fontSize:18,textAlign:'center'}}>      </Text>
+    <Text style ={{fontSize:18,textAlign:'center'}}>      </Text>
+    <Text style ={{fontSize:18,textAlign:'center'}}>      </Text>
+    <Text style ={{fontSize:18,textAlign:'center'}}>      </Text>
     <Text style = {{color: '#0B0B0B',textAlign: 'center'}}>made with care and love by: Group 6</Text>
       </View>
     );
@@ -100,14 +95,14 @@ class ModalScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style ={{fontSize:18,textAlign:'center'}}>Financial literally is a must coming out of </Text>
-        <Text style ={{fontSize:18,textAlign:'center'}}>college. You might say this is why you should get astudent loans,but that </Text>
-        <Text style ={{fontSize:18,textAlign:'center'}}>cost money, why dont you learn something you can apply to your daily life. </Text>
-        <Text style ={{fontSize:18,textAlign:'center'}}>Making more money doesnt outgrow bad money habits.</Text>
+        <Text style ={{fontSize:18,textAlign:'center'}}>Financial literally is a nessecity coming</Text>
+        <Text style ={{fontSize:18,textAlign:'center'}}>out college. Learning financial literacy is an important skill that you can apply</Text>
+        <Text style ={{fontSize:18,textAlign:'center'}}>to your daily life so why not master it with Fin U</Text>
         <Button
           onPress={() => this.props.navigation.goBack()}
-          title="I know why I should use FINLIT"
+          title="I know why I should use FIN U"
         />
+        <Text style ={{fontSize:18,textAlign:'center',fontWeight:'bold'}}>Making more money doesnt outgrow bad money habits.</Text>
       </View>
     );
   }
@@ -135,12 +130,14 @@ class DetailsScreen extends React.Component {
 
     return (
       <View style={{flex: 1, flexDirection: 'column', alignItems: 'stretch', justifyContent: 'center'}}>
-      <Text style ={{fontSize:15,textAlign: 'center'}}>Many college students know how to take in information at a rapid rate. They are so accustomed to only larning about things that apply to school. </Text>
-      <Text style ={{fontSize:15,textAlign: 'center'}}>I will give you a quick overview of how very important functions. </Text>
-      <Text style ={{fontSize:15,textAlign: 'center'}}>Compound Intrest </Text>
+      <Text style ={{fontSize:15,textAlign: 'center'}}>I will give you a quick overview of how very important functions </Text>
+      <Text style ={{fontSize:15,textAlign: 'center'}}>Compound Intrest:</Text>
       <Text style ={{fontSize:15,textAlign: 'center'}}>P=(1+r/n)^nt </Text>
-      <Text style ={{fontSize:15,textAlign: 'center'}}>t= time in years, r= rate, n= times Compounded in a year</Text>
-      <Text style ={{fontSize:15,textAlign: 'center'}}> </Text>
+      <Text style ={{fontSize:15,textAlign: 'center'}}>t= time in years, r= rate, n= times compounded in a year</Text>
+      <Text style ={{fontSize:15,textAlign: 'center',textAlignVertical:'center'}}>Compound Intrest can be used for: savings&deposits/Retirement Savings(401K)</Text>
+      <Text style ={{fontSize:15,textAlign: 'center'}}></Text>
+      <Image source={{uri: 'https://www.thecalculatorsite.com/images/articles/20160630-compound-interest-diagram.jpg'}}
+        style={{ width: 350, height: 200,alignSelf:'center' }}/>
       <TouchableOpacity onPress = {() => {this.props.navigation.navigate('Calculator', {
         itemId: 86,
         otherParam: 'Compound Intrest: Calculator',
@@ -148,42 +145,9 @@ class DetailsScreen extends React.Component {
               <View style = {{height: 100, backgroundColor: '#D96704', alignItems: 'stretch',
                               justifyContent: 'flex-start', borderRadius: 15}}
                      >
-                  <Text style = {{flex: 1,color: 'white',textAlign: 'center',textAlignVertical: 'center'}}>Compound Intrest: Calculator</Text>
+                  <Text style = {{flex: 1,fontSize: 18,color: 'white',textAlign: 'center',textAlignVertical: 'center'}}>Compound Intrest: Calculator</Text>
               </View>
         </TouchableOpacity>
-      </View>
-    );
-  }
-}
-
-class Details2Screen extends React.Component {
-  static navigationOptions = ({ navigation, navigationOptions }) => {
-    const { params } = navigation.state;
-
-    return {
-      title: params ? params.otherParam : 'A Nested Details Screen',
-      /* These values are used instead of the shared configuration! */
-      headerStyle: {
-        backgroundColor: navigationOptions.headerTintColor,
-      },
-      headerTintColor: navigationOptions.headerStyle.backgroundColor,
-    };
-  };
-
-  render() {
-    /* 2. Read the params from the navigation state */
-    const { params } = this.props.navigation.state;
-    const itemId = params ? params.itemId : null;
-    const otherParam = params ? params.otherParam : null;
-
-    return (
-      <View style={{flex: 1, flexDirection: 'column', alignItems: 'stretch',}}>
-      <Text style ={{fontSize:18,textAlign: 'center',fontWeight:'bold',textAlignVertical:'center'}}>Amortization is another important Concept as it allows you to spread out the payments of a loan.</Text>
-      <Text style ={{fontSize:15,textAlign: 'center',textAlignVertical:'center'}}>Monthly Payment: (P*(r/n))/(1-(1+(r/n)^-n*t))</Text>
-      <Text style ={{fontSize:15,textAlign: 'center',textAlignVertical:'center'}}>P=Principal,r=intrest rate,t=number of periods,n=payments per period</Text>
-      <Text style ={{fontSize:15,textAlign: 'center',textAlignVertical:'center'}}>Cash Flow: Income - Expenses(NI)</Text>
-      <Text style ={{fontSize:15,textAlign: 'center',textAlignVertical:'center'}}>Compound Intrest can be used for: savings&deposits/Retirement Savings(401K)</Text>
-      <Text style ={{fontSize:15,textAlign: 'center',textAlignVertical:'center'}}> </Text>
       </View>
     );
   }
@@ -211,17 +175,26 @@ class Details3Screen extends React.Component {
 
     return (
       <View style={{flex: 1, flexDirection: 'column', alignItems: 'stretch',}}>
-      <Text style ={{fontSize:18,textAlign: 'center',fontWeight:'bold',textAlignVertical:'center'}}>If you are really intrested in managing your finances,</Text>
+      <Text style ={{fontSize:18,textAlign: 'center',fontWeight:'bold',textAlignVertical:'center'}}>If you are really intrested in managing your finances</Text>
       <Text style ={{fontSize:15,textAlign: 'center',textAlignVertical:'center'}}>A fincancial advisor recommends:</Text>
       <Text style ={{fontSize:15,textAlign: 'center',textAlignVertical:'center'}}>Taking a look at</Text>
       <Text style ={{fontSize:15,textAlign: 'center',textAlignVertical:'center'}}>Philips Curve:</Text>
+      <View style={{flex: 1, flexDirection: 'row',justifyContent:'space-around'}}>
       <Image source={{uri: 'https://sites.google.com/a/georgetown.edu/nigeriamacro/_/rsrc/1385337397879/phillips/Screen%20Shot%202013-11-24%20at%206.55.51%20PM.png'}}
-        style={{ width: 150, height: 110,alignSelf:'center' }}/>
+        style={{ width: 150, height: 110 }}/>
         <Image source={{uri: 'https://s3-eu-west-1.amazonaws.com/tutor2u-media/subjects/economics/pc_basic_curve.jpg?mtime=20180213123044'}}
-          style={{ width: 150, height: 110,alignSelf:'center' }}/>
+          style={{ width: 150, height: 110 }}/>
+      </View>
+      <Text style ={{fontSize:18,fontWeight:'bold',textAlign: 'center',textAlignVertical:'center'}}>10-Year Treasury Constant Maturity Rate</Text>
+      <Image source={{uri: 'https://fred.stlouisfed.org/graph/fredgraph.png?g=p9aN'}}
+        style={{ width: 150, height: 110,alignSelf:'center' }}/>
       <Text style ={{fontSize:15,textAlign: 'center',textAlignVertical:'center'}}>Formula of Actuarial Method Unearned Intrest Loan</Text>
       <Text style ={{fontSize:15,textAlign: 'center',textAlignVertical:'center'}}>U=(npV)/(100tV)</Text>
       <Text style ={{fontSize:15,textAlign: 'center',textAlignVertical:'center'}}>U= Unearned intrest, p=monthly payments, n=number of remaning monthly payments, V= the value of APR</Text>
+      <Text style ={{fontSize:18,textAlign: 'center',fontWeight:'bold',textAlignVertical:'center'}}>Amortization is another important Concept as it allows you to spread out the payments of a loan.</Text>
+      <Text style ={{fontSize:15,textAlign: 'center',textAlignVertical:'center'}}>Monthly Payment: (P*(r/n))/(1-(1+(r/n)^-n*t))</Text>
+      <Text style ={{fontSize:15,textAlign: 'center',textAlignVertical:'center'}}>P=Principal,r=intrest rate,t=number of periods,n=payments per period</Text>
+      <Text style ={{fontSize:15,textAlign: 'center',textAlignVertical:'center'}}>Cash Flow: Income - Expenses(NI)</Text>
       <Text style ={{fontSize:15,textAlign: 'center',textAlignVertical:'center'}}> </Text>
       </View>
     );
@@ -251,10 +224,26 @@ class Details4Screen extends React.Component {
     return (
       <View style={{flex: 1, flexDirection: 'column', alignItems: 'stretch',justifyContent: 'center'}}>
       <Text style ={{fontSize:18,textAlign: 'center',fontWeight:'bold',textAlignVertical:'center'}}>So since our targetted audience is college students we might as well include tips on how to save money while at university.</Text>
-      <Text style ={{fontSize:15,textAlign: 'center',textAlignVertical:'center'}}>Don't try to save money at the cost of having fun, but also don't eat out every weekend.</Text>
-      <Text style ={{fontSize:15,textAlign: 'center',textAlignVertical:'center'}}>Look at what your university charges you for in other fees and see if you can can get things waived or reduced such as meals plans.</Text>
-      <Text style ={{fontSize:15,textAlign: 'center',textAlignVertical:'center'}}>Try to save by not buying textbooks unless your professor requires you to.</Text>
-      <Text style ={{fontSize:15,textAlign: 'center',textAlignVertical:'center'}}>Don't take out large loans if you don't need them.      Subsidized > Unsubsidized > Private loans</Text>
+      <View style={{alignItems: 'center'}}>
+      <FlatList
+          data={[
+            {key: '•  Know where your money goes'},
+            {key: '   '},
+            {key: '•  Try to save by not buying textbooks unless your professor requires you to.'},
+            {key: '   '},
+            {key: '•  Think about how you are going to repay your loans.'},
+            {key: '   '},
+            {key: '•  Debt isnt nessecary bad- managing debt badly is bad.'},
+            {key: '   '},
+            {key: '•  Build credit by using credit cards or paying loans back on time.'},
+            {key: '•  Think of the oppourtunity cost you took by going to college, make sure you have a good time learning.'},
+            {key: '   '},
+            {key: '•  Dont take out large loans if you dont need them. Subsidized better than Unsubsidized better than Private loans'},
+            {key: '   '},
+            {key: '•  Look at what your university charges you for in other fees and see if you can can get things waived or reduced such as meals plans.'},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}/>
+      </View>
       <Text style ={{fontSize:13,textAlign: 'center',fontWeight:'bold',textAlignVertical:'center'}}>Tips:Updated Daily and sent as a notification</Text>
       </View>
     );
@@ -355,9 +344,6 @@ const RootStack = createStackNavigator(
     MyModal: {
       screen: ModalScreen,
     },
-    Details2: {
-      screen: Details2Screen,
-    },
     Extras: {
       screen: Details3Screen,
     },
@@ -370,9 +356,9 @@ const RootStack = createStackNavigator(
     initialRouteName: 'Home',
     defaultNavigationOptions: {
       headerStyle: {
-        backgroundColor: '#00D72E',
+        backgroundColor: '#ffffff',
       },
-      headerTintColor: '#B35808',
+      headerTintColor: '#D96704',
       headerTitleStyle: {
         fontWeight: 'bold',
       },
